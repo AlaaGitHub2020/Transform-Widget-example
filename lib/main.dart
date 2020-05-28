b479ab5d8db7697638fd5e93e6ec554a6072bcef
Alaa Mashkouk <alaamashkouk2@gmail.com>
2020-05-28 15:01:00 +0530

    Transform Widget example

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:transformexample/my_home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Transform Simple Example',
+    home: MyHome(),
+  ));
+}
---
+++ lib/my_home.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+
+class MyHome extends StatefulWidget {
+  @override
+  _MyHomeState createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: Colors.deepPurpleAccent,
+          title: Text(
+            'Transform Example',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 25.0,
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.bold,
+            ),
+          )),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Transform.translate(
+              child: MyPurpleBox(),
+              offset: Offset(50, 100),
+            ),
+            Transform.rotate(
+              child: MyBlueBox(),
+              angle: 45,
+            ),
+            Transform.scale(
+              child: MyGreenBox(),
+//              alignment: Alignment.center,
+              scale: 1.5,
+            ),
+            Transform(
+              child: MyRedBox(),
+              transform: Matrix4.identity()
+                ..rotateX(45)
+                ..scale(2.0)
+                ..rotateY(45)
+                ..translate(-50.0, -100.0),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget MyYellowBox() {
+    return Container(
+      color: Colors.yellow,
+      width: 25.0,
+      height: 25.0,
+      child: Center(
+        child: Text(
+          'Yellow Box',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 25.0,
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget MyRedBox() {
+    return Container(
+      color: Colors.red,
+      width: 50.0,
+      height: 50.0,
+      child: Center(
+        child: Text(
+          'Red Box',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 25.0,
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget MyBlueBox() {
+    return Container(
+      color: Colors.blue,
+      width: 100.0,
+      height: 100.0,
+      child: Center(
+        child: Text(
+          'Blue Box',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 25.0,
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget MyGreenBox() {
+    return Container(
+      color: Colors.green,
+      width: 150.0,
+      height: 150.0,
+      child: Center(
+        child: Text(
+          'Green Box',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 25.0,
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget MyPurpleBox() {
+    return Container(
+      color: Colors.purple,
+      width: 200.0,
+      height: 200.0,
+      child: Center(
+        child: Text(
+          'Purple Box',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 25.0,
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
